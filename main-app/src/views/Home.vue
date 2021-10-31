102f6f50a947512a98a0847e7a0b6feffdd22096
bolawen <335303383@qq.com>
2021-10-31 16:10:57 +0800

    推送

---
+++ main-app/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>QianKun主应用 Home 页面</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
---
+++ vue-child-app/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app">
+    <h1>QianKun Vue 子应用 首页</h1>
+  </div>
+</template>
